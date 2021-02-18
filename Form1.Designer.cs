34052455327435ae2cd23a31f133480e6584627a
Aleksei Moskaljuk <alekseiko@gmail.com>
2021-02-18 16:12:41 +0100

    completed music extrraction process and transfer to Telegram

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,131 +28,174 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.code_tb = new System.Windows.Forms.TextBox();
-            this.label1 = new System.Windows.Forms.Label();
-            this.authenticate_btn = new System.Windows.Forms.Button();
-            this.connect_btn = new System.Windows.Forms.Button();
-            this.send_btn = new System.Windows.Forms.Button();
-            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.youtube_tb = new System.Windows.Forms.TextBox();
             this.label2 = new System.Windows.Forms.Label();
-            this.button1 = new System.Windows.Forms.Button();
-            this.disconnect_btn = new System.Windows.Forms.Button();
+            this.authenticate_btn = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.code_tb = new System.Windows.Forms.TextBox();
+            this.send_btn = new System.Windows.Forms.Button();
+            this.connect_btn = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.download_btn = new System.Windows.Forms.Button();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
             this.SuspendLayout();
             // 
-            // code_tb
+            // youtube_tb
             // 
-            this.code_tb.Location = new System.Drawing.Point(18, 185);
-            this.code_tb.Name = "code_tb";
-            this.code_tb.Size = new System.Drawing.Size(224, 29);
-            this.code_tb.TabIndex = 0;
-            this.code_tb.Text = "Code from Telegram";
+            this.youtube_tb.Location = new System.Drawing.Point(114, 38);
+            this.youtube_tb.Margin = new System.Windows.Forms.Padding(2);
+            this.youtube_tb.Name = "youtube_tb";
+            this.youtube_tb.Size = new System.Drawing.Size(501, 26);
+            this.youtube_tb.TabIndex = 7;
             // 
-            // label1
+            // label2
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(13, 139);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(243, 25);
-            this.label1.TabIndex = 1;
-            this.label1.Text = "Enter code from Telegram:";
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(5, 38);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(101, 20);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "YouTube link";
             // 
             // authenticate_btn
             // 
-            this.authenticate_btn.Location = new System.Drawing.Point(272, 185);
+            this.authenticate_btn.Enabled = false;
+            this.authenticate_btn.Location = new System.Drawing.Point(9, 202);
+            this.authenticate_btn.Margin = new System.Windows.Forms.Padding(2);
             this.authenticate_btn.Name = "authenticate_btn";
-            this.authenticate_btn.Size = new System.Drawing.Size(156, 38);
+            this.authenticate_btn.Size = new System.Drawing.Size(183, 32);
             this.authenticate_btn.TabIndex = 2;
             this.authenticate_btn.Text = "Connect";
             this.authenticate_btn.UseVisualStyleBackColor = true;
             this.authenticate_btn.Click += new System.EventHandler(this.authenticate_btn_Click);
             // 
-            // connect_btn
+            // label1
             // 
-            this.connect_btn.Location = new System.Drawing.Point(18, 38);
-            this.connect_btn.Name = "connect_btn";
-            this.connect_btn.Size = new System.Drawing.Size(275, 70);
-            this.connect_btn.TabIndex = 4;
-            this.connect_btn.Text = "Send authorization code";
-            this.connect_btn.UseVisualStyleBackColor = true;
-            this.connect_btn.Click += new System.EventHandler(this.connect_btn_Click);
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(5, 131);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(197, 20);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Enter code from Telegram:";
+            // 
+            // code_tb
+            // 
+            this.code_tb.Enabled = false;
+            this.code_tb.Location = new System.Drawing.Point(9, 162);
+            this.code_tb.Margin = new System.Windows.Forms.Padding(2);
+            this.code_tb.Name = "code_tb";
+            this.code_tb.Size = new System.Drawing.Size(184, 26);
+            this.code_tb.TabIndex = 0;
+            this.code_tb.Text = "Code from Telegram";
+            this.code_tb.Enter += new System.EventHandler(this.code_tb_Enter);
             // 
             // send_btn
             // 
-            this.send_btn.Location = new System.Drawing.Point(18, 261);
+            this.send_btn.Location = new System.Drawing.Point(9, 90);
+            this.send_btn.Margin = new System.Windows.Forms.Padding(2);
             this.send_btn.Name = "send_btn";
-            this.send_btn.Size = new System.Drawing.Size(138, 42);
+            this.send_btn.Size = new System.Drawing.Size(113, 35);
             this.send_btn.TabIndex = 6;
             this.send_btn.Text = "Send";
             this.send_btn.UseVisualStyleBackColor = true;
             this.send_btn.Click += new System.EventHandler(this.send_btn_Click);
             // 
-            // textBox2
+            // connect_btn
             // 
-            this.textBox2.Location = new System.Drawing.Point(175, 352);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(612, 29);
-            this.textBox2.TabIndex = 7;
+            this.connect_btn.Location = new System.Drawing.Point(9, 33);
+            this.connect_btn.Margin = new System.Windows.Forms.Padding(2);
+            this.connect_btn.Name = "connect_btn";
+            this.connect_btn.Size = new System.Drawing.Size(221, 58);
+            this.connect_btn.TabIndex = 4;
+            this.connect_btn.Text = "Send authorization code";
+            this.connect_btn.UseVisualStyleBackColor = true;
+            this.connect_btn.Click += new System.EventHandler(this.connect_btn_Click);
             // 
-            // label2
+            // groupBox1
             // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(13, 350);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(127, 25);
-            this.label2.TabIndex = 8;
-            this.label2.Text = "YouTube link";
+            this.groupBox1.Controls.Add(this.connect_btn);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.authenticate_btn);
+            this.groupBox1.Controls.Add(this.code_tb);
+            this.groupBox1.Location = new System.Drawing.Point(15, 22);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(2);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(2);
+            this.groupBox1.Size = new System.Drawing.Size(254, 342);
+            this.groupBox1.TabIndex = 11;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Telegram";
             // 
-            // button1
+            // groupBox2
             // 
-            this.button1.Location = new System.Drawing.Point(827, 350);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(110, 31);
-            this.button1.TabIndex = 9;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
+            this.groupBox2.Controls.Add(this.download_btn);
+            this.groupBox2.Controls.Add(this.progressBar1);
+            this.groupBox2.Controls.Add(this.label2);
+            this.groupBox2.Controls.Add(this.youtube_tb);
+            this.groupBox2.Controls.Add(this.send_btn);
+            this.groupBox2.Location = new System.Drawing.Point(286, 33);
+            this.groupBox2.Margin = new System.Windows.Forms.Padding(2);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Padding = new System.Windows.Forms.Padding(2);
+            this.groupBox2.Size = new System.Drawing.Size(646, 157);
+            this.groupBox2.TabIndex = 12;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "YouTube";
             // 
-            // disconnect_btn
+            // download_btn
             // 
-            this.disconnect_btn.Location = new System.Drawing.Point(509, 38);
-            this.disconnect_btn.Name = "disconnect_btn";
-            this.disconnect_btn.Size = new System.Drawing.Size(203, 70);
-            this.disconnect_btn.TabIndex = 10;
-            this.disconnect_btn.Text = "Disconnect";
-            this.disconnect_btn.UseVisualStyleBackColor = true;
-            this.disconnect_btn.Click += new System.EventHandler(this.disconnect_btn_Click);
+            this.download_btn.Location = new System.Drawing.Point(186, 90);
+            this.download_btn.Margin = new System.Windows.Forms.Padding(2);
+            this.download_btn.Name = "download_btn";
+            this.download_btn.Size = new System.Drawing.Size(113, 35);
+            this.download_btn.TabIndex = 14;
+            this.download_btn.Text = "Download";
+            this.download_btn.UseVisualStyleBackColor = true;
+            this.download_btn.Click += new System.EventHandler(this.download_btn_Click);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(315, 90);
+            this.progressBar1.Margin = new System.Windows.Forms.Padding(2);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(299, 32);
+            this.progressBar1.TabIndex = 13;
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 24F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1210, 502);
-            this.Controls.Add(this.disconnect_btn);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.send_btn);
-            this.Controls.Add(this.connect_btn);
-            this.Controls.Add(this.authenticate_btn);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.code_tb);
+            this.ClientSize = new System.Drawing.Size(950, 387);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.groupBox2);
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
-
-        private System.Windows.Forms.TextBox code_tb;
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Button authenticate_btn;
-        private System.Windows.Forms.Button connect_btn;
-        private System.Windows.Forms.Button send_btn;
-        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox youtube_tb;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button disconnect_btn;
+        private System.Windows.Forms.Button authenticate_btn;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox code_tb;
+        private System.Windows.Forms.Button send_btn;
+        private System.Windows.Forms.Button connect_btn;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Button download_btn;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -1,23 +1,23 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
+﻿using MediaToolkit;
+using MediaToolkit.Model;
+using System;
 using System.Data;
-using System.Drawing;
-using System.Drawing.Text;
+using System.IO;
 using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Windows.Threading;
 using TeleSharp.TL;
 using TeleSharp.TL.Messages;
-using TLSharp;
 using TLSharp.Core;
 using TLSharp.Core.Utils;
+using VideoLibrary;
 
 namespace YoutubeToTelegramMusic
 {
     public partial class Form1 : Form
     {
+        /* variables*/
         private readonly int API_ID = 2148225;
         private readonly string API_HASH = "628a22504e0f925d2d5bd97c62f3863f";
         private readonly string userNumber = "+37255510396";
@@ -25,22 +25,77 @@ namespace YoutubeToTelegramMusic
         private string hash;
         private TLUser user;
 
+        /* form constructor*/
         public Form1()
         {
             InitializeComponent();
+            progressBar1.Minimum = 0;
+            progressBar1.Maximum = 100;
         }
 
-
+        /* GUI control actions*/
         private void connect_btn_Click(object sender, EventArgs e)
         {
             EstablishTelegramConnection();
         }
-
         private void authenticate_btn_Click(object sender, EventArgs e)
         {
             AuthorizeUser();
         }
+        private void send_btn_Click(object sender, EventArgs e)
+        {
+            SendMusicToChannel();
+        }
+        private void download_btn_Click(object sender, EventArgs e)
+        {
+            DownloadFromYoutube();
+        }
 
+        /* Downloads video from YouTube and converts it to .mp3 file 
+           @return local path to converted .mp3 file */
+        private string DownloadFromYoutube()
+        {
+            /* TODO: Check for youtube link field */
+            if (string.IsNullOrWhiteSpace(youtube_tb.Text))
+            {
+                MessageBox.Show("Please enter a valid YouTube link");
+                return "";
+            }
+
+            var youtube = YouTube.Default;
+            var vid = youtube.GetVideo(youtube_tb.Text);
+            File.WriteAllBytes(vid.FullName, vid.GetBytes());
+
+            var inputFile = new MediaFile { Filename = vid.FullName };
+            var outputFile = new MediaFile { Filename = vid.FullName.Replace(".mp4", ".mp3") };
+
+            using (var engine = new Engine())
+            {
+                engine.GetMetadata(inputFile);
+
+                engine.Convert(inputFile, outputFile);
+
+                if (InvokeRequired)
+                {
+                    this.Invoke((MethodInvoker)delegate
+                    {
+                        engine.ConvertProgressEvent += (sender, args) => progressBar1.Value = Convert.ToInt32(args.ProcessedDuration.TotalMinutes / args.TotalDuration.TotalMinutes * 100 * 0.85);
+                    });
+                }
+
+
+                // show download progress in the progress bar
+            }
+
+            // Register the progress events. We treat the download progress as 85% of the progress and the extraction progress only as 15% of the progress,
+            // because the download will take much longer than the audio extraction.
+            //audioDownloader.DownloadProgressChanged += (sender, args) => progressBar1.Value = Convert.ToInt32(args.ProgressPercentage * 0.85);
+            //audioDownloader.AudioExtractionProgressChanged += (sender, args) => progressBar1.Value = Convert.ToInt32(args.ProgressPercentage * 0.15);
+
+            return outputFile.Filename;
+        }
+
+        /* Send authorization code request to Telegram */
         private async void EstablishTelegramConnection()
         {
             try
@@ -48,9 +103,11 @@ namespace YoutubeToTelegramMusic
                 this.client = new TelegramClient(API_ID, API_HASH);
                 await client.ConnectAsync();
 
-                System.Threading.Thread.Sleep(1000);
-
                 this.hash = await client.SendCodeRequestAsync(userNumber);
+
+                // enable controls to enter received code 
+                code_tb.Enabled = true;
+                authenticate_btn.Enabled = true;
 
             } catch (Exception ex)
             {
@@ -58,63 +115,71 @@ namespace YoutubeToTelegramMusic
             } 
         }
 
+        /* Authorize the user with the received code */
         private async void AuthorizeUser()
         {
             string code = code_tb.Text;
 
             if (!(string.IsNullOrWhiteSpace(code) || string.IsNullOrWhiteSpace(hash) || code == "Code from Telegram"))
             {
-                //if (!client.IsUserAuthorized())
-                //{
+                try
+                {
                     this.user = await client.MakeAuthAsync(userNumber, hash, code);
                     MessageBox.Show(user.Status.ToString());
-                //}
+
+                } catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
             }
             else MessageBox.Show("Please check your input!");
         }
 
+        /* This function accepts downloaded music from YouTube and sends it to Telegram channel */
         private async void SendMusicToChannel()
         {
-            if (user == null) return;
+            if (user == null)
+            {
+                MessageBox.Show("Telegram user is not authorized");
+                return;
+            }
 
             var dialogs = (TLDialogs)await client.GetUserDialogsAsync();
 
-            // find chat by title
+            // find Telegram channel by name
             TLChannel chat = dialogs.Chats.Where(c => c.GetType() == typeof(TLChannel)).Cast<TLChannel>().FirstOrDefault(c => c.Title == "Music");
 
-            string filePath = "test.mp3";
-            var fileResult = await client.UploadFile(filePath, new System.IO.StreamReader(filePath));
+            string filePath = this.DownloadFromYoutube();
+
+            if (string.IsNullOrEmpty(filePath)) return;
+
+            // upload file
+            var fileResult = await client.UploadFile(filePath, new StreamReader(filePath));
 
             // create an attribute for a file
             TLDocumentAttributeFilename s = new TLDocumentAttributeFilename
             {
                 FileName = filePath.Replace(".mp3", "")
             };
-            TLVector<TLAbsDocumentAttribute> attributes = new TLVector<TLAbsDocumentAttribute>();
-            attributes.Add(s);
+            TLVector<TLAbsDocumentAttribute> attributes = new TLVector<TLAbsDocumentAttribute>
+            {
+                s
+            };
 
             // finally, send the file to the channel
-            await client.SendUploadedDocument(
+            client.SendUploadedDocument(
                 new TLInputPeerChannel() { ChannelId = chat.Id, AccessHash = (long)chat.AccessHash },
                 fileResult,
                 "",
                  "audio/mpeg",
                  attributes);
-        }
-
-        private void send_btn_Click(object sender, EventArgs e)
-        {
-            SendMusicToChannel();
-        }
-
-        private void textBox1_TextChanged(object sender, EventArgs e)
-        {
 
         }
 
-        private void disconnect_btn_Click(object sender, EventArgs e)
+        /* Clean up code field when it is in focus*/
+        private void code_tb_Enter(object sender, EventArgs e)
         {
-            client.Dispose();
+            code_tb.Text = "";
         }
     }
 }
